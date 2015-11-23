7ace6ec84a32e7542327b0ec1d69a57ef640ee7b
stevesteve <steve.l.exe@gmail.com>
2015-11-23 11:43:59 +0100

    added api/tasks get and post routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,32 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('tasks');
+});
+
+Route::group(['prefix' => 'api'], function () {
+	Route::get('/tasks', function () {
+		$tasks = Task::get();
+		return response()->json($tasks);
+	});
+
+	Route::post('/tasks', function (Request $request) {
+		$validator = Validator::make($request->all(), [
+			'name' => 'required | max:255',
+		]);
+
+		if ($validator->fails()) {
+			return response()->json($validator->messages(),400);
+		}
+
+		$task = new Task();
+		$task->name = $request->name;
+		$task->save();
+
+		return response()->json($task);
+	});
 });
