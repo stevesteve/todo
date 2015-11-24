7217a84cfaa16cc907d5c26e97cda5c28dbf691c
stevesteve <steve.l.exe@gmail.com>
2015-11-24 18:24:58 +0100

    First version of the page style
    I added the compiled css file to make the installation more convenient

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,32 +3,38 @@
 @section('content')
 
 	<div id="listspanel" ng-controller="ListsCtrl">
-		<form ng-submit="create()">
+		<form ng-submit="create()" id="list-form" class="compact-text-form">
 			<input type="text" ng-model="newList.name" placeholder="New List">
-			<button type="submit">Create</button>
+			<button type="submit">+</button>
 			<ul class="errorlist">
 				<li ng-repeat="error in creationerrors">
 					@{{ error }}
 				</li>
 			</ul>
 		</form>
-		<ul>
-			<li ng-repeat="list in lists | orderBy:'name'" ng-click="setActiveList()">
+		<ul id="listlist">
+			<li ng-repeat="list in lists | orderBy:'name'" ng-click="setActiveList()"
+				ng-class="{active: activeList.id === list.id}">
+
 				@{{ list.name }}
-				<button ng-click="delete($event)">Delete</button>
+				<button ng-click="delete($event)" class="btn-delete">x</button>
 			</li>
 		</ul>
 	</div>
 	<div id="taskspanel" ng-if="activeList" ng-controller="TasksCtrl">
-		Show Done: <input type="checkbox" ng-model="filterTasks.done" ng-true-value="undefined" ng-false-value="0">
-		<form ng-submit="create()">
+		<form ng-submit="create()" id="task-form" class="compact-text-form">
 			<input type="text" ng-model="newTask.name" placeholder="New Task">
-			<button type="submit">Create</button>
+			<button type="submit">+</button>
 			<ul class="errorlist">
 				<li ng-repeat="error in creationerrors">@{{ error }}</li>
 			</ul>
 		</form>
-		<ul>
+		<label for="show-done" class="checkbox-label">
+			<span>Show Done: </span>
+			<input id="show-done" type="checkbox"
+				ng-model="filterTasks.done" ng-true-value="undefined" ng-false-value="0">
+		</label>
+		<ul id="tasklist">
 			<li ng-repeat="task in filteredTasks = (tasks | filter:filterTasks | orderBy:['!done','id']:true)">
 				<input type="checkbox"
 					ng-model="task.done"
@@ -36,7 +42,7 @@
 					ng-false-value="0"
 					ng-change="task.$save()">
 				@{{ task.name }}
-				<button ng-click="delete($event)">Delete</button>
+				<button ng-click="delete($event)" class="btn-delete">x</button>
 			</li>
 		</ul>
 		<div ng-if="filteredTasks.length === 0">No Tasks</div>
